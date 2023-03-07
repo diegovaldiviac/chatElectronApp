0cfdc6b6f4542f8fd12b378f4b94a509d344a41b
Diego <valdiviacox.d@northeastern.edu>
2023-03-06 23:51:47 -0500

    app styling - preload

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
 import App from "./App";
 
-const container = document.getElementById("html-body-div") as HTMLElement
+// BOOTSTRAP
+const container = document.getElementById("html-body-div") as HTMLElement;
 const root = ReactDOM.createRoot(container);
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-    );
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
